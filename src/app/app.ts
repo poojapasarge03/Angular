3b5d8072709d2eb0a4cc229b18e8c07dbb5b9421
poojapasarge03 <chaptipooja@gmail.com>
2026-09-12 21:50:40 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Profile } from './profile/profile';
+import { Navbar } from './navbar/navbar';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [Profile, Navbar],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('angular-app');
+  name = "Pooja"
+  age = 20
+
+  updateName(){
+    this.name = "Pasarge";
+  }
+
+  callme(){
+    alert("Button clicked")
+  }
+
+  sayHello(){
+    console.log("HEllo Guys")
+  }
 }
